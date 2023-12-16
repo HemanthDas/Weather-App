e99b2c511a84a6ebb759f91208b209eb41857e85
HemanthDas <hemanthchoudary4@gmail.com>
2023-12-16 12:33:00 +0530

    Update Bottom.jsx App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Top from "./components/top";
+import Bottom from "./components/bottom";
 function App() {
   return (
     <>
-      
-      
+      <Top />
+      <Bottom />
     </>
   );
 }
