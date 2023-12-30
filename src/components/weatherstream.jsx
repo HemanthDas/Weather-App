8f1ab75983349a7dcd2311a3d3a9733326a74432
HemanthDas <hemanthchoudary4@gmail.com>
2023-12-30 20:34:17 +0530

    Last step to complete

--- src/components/weatherstream.jsx
+++ src/components/weatherstream.jsx
@@ -2,9 +2,13 @@ import PropTypes from "prop-types";
 const WeatherStream = (main) => {
   return (
     <div id="weather-info">
-      <div>Min Temperature: {main.main.temp_min} K</div>
-      <div>Max Temperature: {main.main.temp_max} K</div>
-      <div>Humidity: {main.main.humidity}%</div>
+      <div className="weather-ino-box">
+        Min Temperature: {main.main.temp_min} K
+      </div>
+      <div className="weather-ino-box">
+        Max Temperature: {main.main.temp_max} K
+      </div>
+      <div className="weather-ino-box">Humidity: {main.main.humidity}%</div>
     </div>
   );
 };
